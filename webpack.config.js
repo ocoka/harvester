4daf05f439b9463a9d311fb51f380fcefdcea146
Ocoka <somewhere@network>
2020-01-01 13:10:58 +0300

    create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+//return config
+module.exports = (env, argv) => {
+  console.log(`Current mode is ${argv.mode || "development"}`)
+  return {
+
+    // stats: 'verbose',
+    stats: 'normal',
+    mode: argv.mode || 'development',
+    entry: {
+      main: './src/index',
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename:
+        argv.mode === 'production' ? '[name].[contenthash].js' : '[name].js',
+    },
+
+    resolve: {
+      alias: { '@': path.resolve(__dirname, 'src/') },
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.styl', '.scss'],
+    },
+
+    devtool: argv.mode === 'production' ? false : 'module-nosources-source-map',
+
+    module: {
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'all',
+          },
+          libs: {
+            test: /src\/libs\//,
+            name: 'libs',
+            chunks: 'all',
+          },
+        },
+      },
+      runtimeChunk: 'single',
+      noEmitOnErrors: true,
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename:
+          argv.mode === 'production'
+            ? '[name].[contenthash].css'
+            : '[name].css',
+      }),
+      new CheckerPlugin(),
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      port: 9000
+    }
+  };
+};
