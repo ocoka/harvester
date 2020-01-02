14755ae03304096acec5398641c3b6382509720a
Ocoka <somewhere@network>
2020-01-02 12:22:03 +0300

    all application will be splitted in to available "actions" with it's own component

--- src/domainmodel/arch/interfaces.ts
+++ src/domainmodel/arch/interfaces.ts
@@ -1,5 +1,14 @@
 import * as React from 'react';
 
+/* this file contains all simple interfaces for models, it's like simple structures */
+export interface AvailableAction {
+  id: string;
+  name: string;
+  icon?: string;
+  content: React.FunctionComponentFactory<any> | undefined | null
+}
+export type AvailableActions = AvailableAction[];
+
 export interface Bookmark {
   title: string;
   url: string | undefined;
