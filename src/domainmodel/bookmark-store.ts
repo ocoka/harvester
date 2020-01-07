8a7aacde2e6f9186d4cd135acecbf084cd26d619
Ocoka <somewhere@network>
2020-01-07 15:16:00 +0300

    state keeper for Chrome bookmarks as flat list

---
+++ src/domainmodel/bookmark-store.ts
@@ -0,0 +1,25 @@
+import { Bookmark } from './arch/interfaces';
+import { Store } from './arch/abstract-store';
+import { serialIterator } from '@/utility/iterator';
+import { Subject } from 'rxjs';
+export class BookmarkStore extends Store<Bookmark[]> {
+    constructor() {
+      super(new Subject<Bookmark[]>())
+    }
+    static getInstance() {
+      if (!BookmarkStore.instances.has(BookmarkStore)) {
+        BookmarkStore.instances.set(BookmarkStore, new BookmarkStore());
+      }
+      this.addHolder(BookmarkStore, this.getInstance);
+      return BookmarkStore.instances.get(BookmarkStore)!;
+    }
+    requestBookmarks() {
+      chrome.bookmarks.getTree(_ => {
+        const flatBookmarks: chrome.bookmarks.BookmarkTreeNode[] = Array.from(serialIterator<chrome.bookmarks.BookmarkTreeNode>(_, ['children']));
+        this.setState(flatBookmarks.map(_ => ({
+          title: _.title,
+          url: _.url
+        })));
+      })
+    }
+}
