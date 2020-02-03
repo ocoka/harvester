4c600da91a6f9ada3147c4b2d5a5144c2d18ac8f
Ocoka <somewhere@network>
2020-02-03 13:27:31 +0300

    UI control react rendering tracing

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -5,15 +5,18 @@ import { HiLuxRef } from '@/components/utils/hilux';
 import { loadDefaultTheme, changeTheme } from '@/services/theme_loader2';
 import './app.scss';
 import { VmWorkbench } from './vm/workbench';
+import { CoSwitchLightMemo, CoSwitchMemo } from './shared/switch';
 
 loadDefaultTheme();
 export function CoApp() {
+  const themes = ['main', 'forest', 'white'];
+  const [trace, setTrace] = useState(window.reactorTrace);
   const myRef = useRef<HTMLDivElement>(null)
   HiLuxRef('app', myRef);
-  const themes = ['main', 'forest', 'white'];
   return (
     <div className='app o-centered' ref={myRef}>
       <div className='app__header o-centered__cont'>
+        -= Bookmark Harvester ( ver: 0.0.1.pre-alpha ) =-
       </div>
       <div className='app__content o-centered__cont o-centered__cont_full'>
         <VmWorkbench/>
@@ -22,6 +25,10 @@ export function CoApp() {
         <section>
           <h6 className='c-fan-head'>Available color themes:</h6>
           {themes.map(_ => <span className='app__theme-click' onClick={() => { changeTheme(_) }} key={_}>{_}</span>)}
+        </section>
+        <section>
+          <h6 className='c-fan-head'>React Render Tracing:</h6>
+          <CoSwitchMemo value={trace} onChange={(newVal) => { console.log(`Tracing state: ${newVal}`); window.reactorTrace = newVal; setTrace(newVal) }} />
         </section>
       </div>
     </div>
